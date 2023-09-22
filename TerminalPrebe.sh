4284f35552107e69c2c87c124264b606aff2cc70
franc <francisco.reynoso2000@gmail.com>
2023-09-22 00:41:55 -0600

    Se resolvieron los conflictos en TerminalPrebe.sh

--- TerminalPrebe.sh
+++ TerminalPrebe.sh
@@ -11,6 +11,7 @@ trap ' ' SIGINT SIGTERM #Evita que las señales mandadas por ctrl+c y ctrl+z sea
             reset="\033[0m"
 
 
+<<<<<<< HEAD
 InfoSistema(){
     echo "Información del sistema :)"
     echo
@@ -47,6 +48,113 @@ InfoSistema(){
     if [ -x "$(command -v lsb_release)" ]; then
         version_so=$(lsb_release -d | awk -F ":\t" '{print $2}')
         echo "Versión del sistema operativo: $version_so"
+=======
+# Comprueba si el usuario existe en el sistema
+if id "$usuario" &>/dev/null; then
+    # Intenta autenticar al usuario usando su contraseña
+    if su -c "exit" - "$usuario" <<< "$contrasena" 2>/dev/null; then
+        echo "Inicio de sesión exitoso para el usuario $usuario"
+
+        # Colores de texto
+        rojo="\033[31m"
+        verde="\033[32m"
+        amarillo="\033[33m"
+        azul="\033[34m"
+        magenta="\033[35m"
+        cian="\033[36m"
+        blanco="\033[37m"
+        reset="\033[0m"
+        echo -e "${amarillo}BBBBBBBB    II  EEEEEEEE    NNN     NN    VV       VV   EEEEEEEE   NNN     NN     II   DDDD      OOOO   II ${reset}"
+        echo -e "${azul}BB    BB    II  EE          NN N    NN     VV     VV    EE         NN N    NN     II   D   D    O    O  II ${reset}"
+        echo -e "${verde}BBBBBB      II  EEEEEEE     NN  N   NN      VV   VV     EEEEEEE    NN  N   NN     II   D    D   O    O  II ${reset}"
+        echo -e "${rojo}BB   BB     II  EE          NN   N  NN       VV VV      EE         NN   N  NN     II   D   D    O    O     ${reset}"
+        echo -e "${magenta}BBBBBBBB    II  EEEEEEEE    NN    NNNN        VV        EEEEEEEE   NN    NNNN     II   DDDD      OOOO   O  ${reset}"
+        
+        while true; do
+            # Obtener la carpeta actual
+            carpeta_actual=$(pwd)
+
+            echo
+            echo -e "${cian}Carpeta actual: $carpeta_actual ${reset}"
+            echo
+            echo -e "${cian}Bienvenido $usuario, ¿Qué deseas hacer? ${reset}"
+            echo
+            echo " Escribe 'Ayuda' para ver una lista de comandos disponibles :)"
+            echo " Escribe 'infosis' para ver la información del sistema :)"
+            echo " Escribe 'time' para ver la hora actual :)"
+            echo " Escribe 'find' para buscar un archivo en un directorio debes pasar 2 parametros:)"
+            echo " Escribe 'juegos' para ver nuestros juegos :)"
+            echo " Escribe 'creditos' para ver los créditos :)"
+            echo " Escribe 'exit' para salir :("
+            echo 
+            echo
+
+            # Leer la opción del usuario
+            read -p "Ingresa la palabra clave de la opción: " opcion
+
+            # Evaluar la opción
+            case $opcion in
+                "Ayuda")
+                    echo "Listado de comandos disponibles :)" 
+                    echo "Ayuda: Muestra una lista de los comandos disponibles :)"
+                    echo "infosis: Muestra la información del sistema :)"
+                    echo "time: Muestra la hora actual :)"
+                    echo "find: Busca un archivo en un directorio :)"
+                    echo "creditos: Muestra los créditos :)"
+                    echo "juegos: Muestra nuestros juegos :)"
+                    echo "exit: Sale del sistema :("
+                    echo 
+                    ;;
+                
+                "infosis")
+                    source infosis.sh
+                    
+                    ;;
+    
+                "time")
+                    source time.sh
+                    ;;
+    
+                "find")
+                    echo "Debes ingresar los parámetros de la siguiente manera: /ruta/a/mi/directorio miarchivo.txt"
+                    # Leer el directorio y el nombre del archivo
+                    read -p "Ingresa el directorio: " directorio
+                    read -p "Ingresa el nombre del archivo: " archivo_a_buscar
+
+                    # Utiliza el comando find para buscar el archivo en el directorio especificado
+                    resultado=$(find "$directorio" -name "$archivo_a_buscar")
+
+                    # Verifica si el archivo fue encontrado
+                    if [ -n "$resultado" ]; then
+                        echo "El archivo $archivo_a_buscar fue encontrado en el directorio $directorio."
+                    else
+                        echo "El archivo $archivo_a_buscar no fue encontrado en el directorio $directorio."
+                    fi
+                    ;;
+                "creditos")
+                    source creditos.sh
+                    ;;
+
+                "juegos")
+                source juegos.sh
+                   
+                   
+                   ;;
+
+                "exit")
+                    echo "Saliendo del sistema :("
+                    exit 0 
+                    ;;
+
+                *)
+                    echo "Opción no válida :("
+                    ;;
+            esac
+            
+        done
+    else
+        echo "Contraseña incorrecta para el usuario $usuario"
+>>>>>>> cb8a337 ([ADD][FRANK] Agregue los juegos y agregue imagenes)
     fi
 }
 
--- tempCodeRunnerFile.sh
+++ tempCodeRunnerFile.sh
@@ -1,102 +1,24 @@
-#!/bin/bash
+            # Genera un número aleatorio
+            numero=$((RANDOM % 100 + 1))
 
-echo "Hola, por favor ingresa tu nombre de usuario:"
-read usuario
-echo "Contraseña:"
-read -s contrasena
+            # Bucle de juego
+            for i in {1..10}; do
+            # Solicitar un número
+            echo "Adivina el número mágico (1-100): "
+            numero_jugador=$(read -p "Tu número: ")
 
-# Comprueba si el usuario existe en el sistema
-if id "$usuario" &>/dev/null; then
-    # Intenta autenticar al usuario usando su contraseña
-    if su -c "exit" - "$usuario" <<< "$contrasena" 2>/dev/null; then
-        echo "Inicio de sesión exitoso para el usuario $usuario"
+            # Comprobar si el número es correcto
+            if [ "$numero_jugador" = "$numero" ]; then
+                echo "¡Ganaste!"
+                break
+            elif [ "$numero_jugador" -lt "$numero" ]; then
+                echo "El número es mayor"
+            else
+                echo "El número es menor"
+            fi
+            done
 
-        # Colores de texto
-        rojo="\033[31m"
-        verde="\033[32m"
-        amarillo="\033[33m"
-        azul="\033[34m"
-        magenta="\033[35m"
-        cian="\033[36m"
-        blanco="\033[37m"
-        reset="\033[0m"
-        echo -e "${amarillo}BBBBBBBB    II  EEEEEEEE    NNN     NN    VV       VV   EEEEEEEE   NNN     NN     II   DDDD      OOOO   II ${reset}"
-        echo -e "${azul}BB    BB    II  EE          NN N    NN     VV     VV    EE         NN N    NN     II   D   D    O    O  II ${reset}"
-        echo -e "${verde}BBBBBB      II  EEEEEEE     NN  N   NN      VV   VV     EEEEEEE    NN  N   NN     II   D    D   O    O  II ${reset}"
-        echo -e "${rojo}BB   BB     II  EE          NN   N  NN       VV VV      EE         NN   N  NN     II   D   D    O    O     ${reset}"
-        echo -e "${magenta}BBBBBBBB    II  EEEEEEEE    NN    NNNN        VV        EEEEEEEE   NN    NNNN     II   DDDD      OOOO   O  ${reset}"
-
-        # Obtener la carpeta actual
-        carpeta_actual=$(pwd)
-
-        echo -e "${cian}Carpeta actual: $carpeta_actual ${reset}"
-        echo -e "${cian}Bienvenido $usuario, ¿Qué deseas hacer? ${reset}"
-
-        # Mostrar opciones al usuario
-        echo " Escribe 'Ayuda' para ver una lista de comandos disponibles :)"
-        echo " Escribe 'infosis' para ver la información del sistema :)"
-        echo " Escribe 'time' para ver la hora actual :)"
-
-        # Leer la opción del usuario
-        read -p "Ingresa la palabra clave de la opción: " opcion
-
-        # Evaluar la opción
-        case $opcion in
-            "Ayuda")
-                echo "Listado de comandos disponibles :)" ;;
-            
-            "infosis")
-                echo "Información del sistema :)"
-                echo
-                echo "Nombre del equipo: $(hostname)"
-                echo
-                echo 
-                echo "Dirección IP: $(hostname -I)"
-                echo
-                echo
-                echo "Dirección MAC: $(ip link show | awk '/ether/ {print $2}')"
-                echo
-                echo
-                echo "Fecha y hora: $(date)"
-                echo
-                echo
-                echo "Versión del kernel: $(uname -r)"
-                echo
-                echo
-                echo "Usuarios conectados: $(who -q)"
-                echo
-                echo
-                echo "Memoria RAM disponible: $(free -h)"
-                echo
-                echo
-                echo "Espacio en disco: $(df -h)"
-                echo
-                echo
-                # Obtener la arquitectura del sistema
-                arquitectura=$(arch)
-                echo "Arquitectura del sistema: $arquitectura"
-                echo
-                echo
-                # Obtener la versión del sistema operativo (requiere el paquete lsb-release)
-                if [ -x "$(command -v lsb_release)" ]; then
-                    version_so=$(lsb_release -d | awk -F ":\t" '{print $2}')
-                    echo "Versión del sistema operativo: $version_so"o
-                
-                fi
-                ;;
-
-            "time")
-                # Obtiene la hora actual
-                time=$(timedatectl -f "%d %B %Y %H:%M:%S")
-
-                # Muestra la hora actual
-                echo "La hora actual es: $time"
-            *)
-                echo "Opción no válida :(" ;;
-        esac
-    else
-        echo "Contraseña incorrecta para el usuario $usuario"
-    fi
-else
-    echo "El usuario $usuario no existe en el sistema"
-fi
+            # Mostrar el resultado
+            if [ "$numero_jugador" != "$numero" ]; then
+            echo "¡Perdiste!"
+            fi
