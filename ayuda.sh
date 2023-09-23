af66a42a581a114b89ffac082fb5b60817ee112b
Francisco Javier Reynoso Ortega <136301116+FranciscoJRO@users.noreply.github.com>
2023-09-22 23:27:59 -0600

    Update ayuda.sh

--- ayuda.sh
+++ ayuda.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!bin/bash
 trap ' ' 2 20
 
-echo "Listado de comandos disponibles :)" 
-echo "Ayuda: Muestra una lista de los comandos disponibles :)"
-echo "infosis: Muestra la información del sistema :)"
-echo "time: Muestra la hora actual :)"
-echo "find: Busca un archivo en un directorio :)"
-echo "creditos: Muestra los créditos :)"
-echo "juegos: Muestra nuestros juegos :)"
-echo "exit: Sale del sistema :("
-echo
+            echo " Escribe 'Ayuda' para ver una lista de comandos disponibles :)"
+            echo " Escribe 'infosis' para ver la información del sistema :)"
+            echo " Escribe 'time' para ver la hora actual :)"
+            echo " Escribe 'find' para buscar un archivo en un directorio debes pasar 2 parametros:)"
+            echo " Escribe 'juegos' para ver nuestros juegos :)"
+            echo " Escribe 'creditos' para ver los créditos :)"
+            echo " Escribe 'musica' para ver los comandos de música :)"
+            echo " Escribe 'exit' para salir :("
 
 trap - 2 20
